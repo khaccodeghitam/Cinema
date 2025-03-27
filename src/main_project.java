c6af94e0ef4e77ccf5bc952979adbdd3ccb13025
khaccodeghitam <3123410357@sv.sgu.edu.vn>
2025-03-27 20:56:45 +0700

    fix main

--- src/main_project.java
+++ src/main_project.java
@@ -12,29 +12,29 @@ import ql_cinema.register;
 import javax.swing.*;
 import java.awt.*;
 public class main_project {
-     public static void main(String[] args) {
+      public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
-            // Tạo giao diện login
+          
             login loginFrame = new login();
             
             // Đặt vị trí của login form ra giữa màn hình
             loginFrame.setLocationRelativeTo(null);
             
-            // Hiển thị login form
+           
             loginFrame.setVisible(true);
             
-            // Thêm sự kiện cho nút Đăng Ký
+      
             loginFrame.jButton2.addActionListener(e -> {
-                // Tạo giao diện register
+                
                 register registerFrame = new register();
                 
-                // Đặt vị trí của register form ra giữa màn hình
+                
                 registerFrame.setLocationRelativeTo(null);
                 
-                // Hiển thị register form
+            
                 registerFrame.setVisible(true);
                 
-                // Ẩn login form
+               
                 loginFrame.setVisible(false);
             });
         });
