23fafc761a92c7d0e4999f75113673452167cf72
khaccodeghitam <3123410357@sv.sgu.edu.vn>
2025-03-31 14:31:11 +0700

    rename

---
+++ QL_Cinema/src/main_project.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+import GUI.login;
+import GUI.register;
+import javax.swing.*;
+import java.awt.*;
+public class main_project {
+      public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+          
+            login loginFrame = new login();
+            
+            // Đặt vị trí của login form ra giữa màn hình
+            loginFrame.setLocationRelativeTo(null);
+            
+           
+            loginFrame.setVisible(true);
+            
+      
+            loginFrame.jButton2.addActionListener(e -> {
+                
+                register registerFrame = new register();
+                
+                
+                registerFrame.setLocationRelativeTo(null);
+                
+            
+                registerFrame.setVisible(true);
+                
+               
+                loginFrame.setVisible(false);
+            });
+        });
+    }
+}
